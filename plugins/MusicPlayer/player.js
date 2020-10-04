6b21f77b5257dbfae9ad67798c4b593763169a2b
Adam <alloudsounds@gmail.com>
2020-10-04 13:59:52 -0700

    Revert "Fixes for music player stop command."
    This reverts commit bcda971d94012b5be37bb5a69bd023c391d29274.

--- plugins/MusicPlayer/player.js
+++ plugins/MusicPlayer/player.js
@@ -295,7 +295,7 @@ exports.stop = {
 			queue.splice(0);
 		}
 
-		if (voiceConnection.player.dispatcher) voiceConnection.player.dispatcher.end();
+		//voiceConnection.player.dispatcher.end();
 		return msg.channel.send(wrap('Queue has been cleared.'));
 	}
 }
@@ -313,7 +313,7 @@ function executeQueue(client, msg, queue) {
 
 			// Leave the voice channel.
 			const voiceConnection = client.voiceConnections.get(msg.guild.id);
-			if (voiceConnection.player.dispatcher != null) {
+			if (voiceConnection != null) {
 				voiceConnection.player.dispatcher.end();
 				voiceConnection.channel.leave();
 				return;
